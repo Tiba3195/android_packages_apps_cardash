21f3e9c0978a1bd620f90ace5a32cbf6f252886d
Tiba3195 <justin_beast@hotmail.com>
2023-12-28 22:47:43 +1100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Repainter
-LOCAL_SRC_FILES := Repainter.apk
+LOCAL_MODULE := Lawnchair
+LOCAL_SRC_FILES := Lawnchair.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
